a59c22cade3c3139a5420aba3b70d6cf6b40f9d9
Natali kruk <natasha_505@yahoo.com>
2019-10-04 03:08:17 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,35 +19,8 @@ def my_hash_creator(key, value)
 end 
 
 def read_from_hash(hash, key)
-  
+  hash[key]
 end 
 
 
 
-def read_from_hash(hash, key)
-    my_hash = {
-    :key_one => "value one",
-    :key_two => "value two",
-    :key_three => "value three"
-  }
-  
-  if my_hash[:key_four]
-    puts "This is the value."
-  else
-    puts "No key found!"
-end
-
-
-def update_counting_hash(hash, key)
-   random_hash = {
-    :value_one => 1,
-    :value_two => 2,
-    :value_three => 3
-  }
-  
-  if random_hash[:value_four]
-    random_hash[:value_four] += 1 
-  else 
-    random_hash[:value_four] = 1 
-  }
-end
